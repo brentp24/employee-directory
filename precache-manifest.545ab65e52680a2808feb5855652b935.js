b70b304b9988d887841373676226cb0cbc847aa9
Brent Peterson <brentp24@gmail.com>
2020-07-17 10:14:55 -0600

    Updates

---
+++ precache-manifest.545ab65e52680a2808feb5855652b935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3022fa6584995e38f98152569531f8e5",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "2a2531b2f4ffcedf3760",
+    "url": "/demo-app/static/css/main.aa523dc6.chunk.css"
+  },
+  {
+    "revision": "77f9d24ddac28f6f3a13",
+    "url": "/demo-app/static/js/2.87fc6441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-app/static/js/2.87fc6441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2531b2f4ffcedf3760",
+    "url": "/demo-app/static/js/main.03245f61.chunk.js"
+  },
+  {
+    "revision": "b92c899f164488c01bff",
+    "url": "/demo-app/static/js/runtime-main.e63db1da.js"
+  }
+]);
